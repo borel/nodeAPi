45d03bf57a237e2a790bcf452e75866ba8928bf3
pbborel <pbborel@gmail.com>
2015-08-25 00:40:53 +0200

    Heroku commit

--- sample.js
+++ sample.js
@@ -4,19 +4,19 @@ var mongoose = require('mongoose');
 var mongodb = require('mongodb');
 
 // mongoose connect
-mongoose.connect('mongodb://localhost:27017/borel');
-
-
-var userSchema = new mongoose.Schema({
-  _id     : Number,
-  firstName: String,
-  lastName: String,
-  birthDate: Date,
-  sex: Number,
-  updated_at: { type: Date, default: Date.now },
-});
-
-var User = mongoose.model('User', userSchema);
+// mongoose.connect('mongodb://localhost:27017/borel');
+//
+//
+// var userSchema = new mongoose.Schema({
+//   _id     : Number,
+//   firstName: String,
+//   lastName: String,
+//   birthDate: Date,
+//   sex: Number,
+//   updated_at: { type: Date, default: Date.now },
+// });
+//
+// var User = mongoose.model('User', userSchema);
 
 // var user = new User({_id:0,firstName: 'Pauline', lastName: 'Borel', date: '20/02/1985'});
 //
@@ -66,15 +66,21 @@ router.get('/', function(req, res) {
 });
 
 // more routes for our API will happen here
-router.route('/users')
+// router.route('/users')
+//   // gel all user
+//   .get(function (req, res) {
+//     User.find(function (err, users) {
+//       if (err) return console.error(err);
+//         res.json(users);
+//     });
+//   });
+
+router.route('/user/borel')
   // gel all user
   .get(function (req, res) {
-    User.find(function (err, users) {
-      if (err) return console.error(err);
-        res.json(users);
-    });
+    var user = {'fistName':'PB', 'lastName':'Borel'};
+      res.json(user);
   });
-
 
   // REGISTER OUR ROUTES -------------------------------
   // all of our routes will be prefixed with /api
