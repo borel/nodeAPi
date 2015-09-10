ae6c839e97890234f11163d037c3ca050f3e677e
unknown <pbborel@CZS004.cityzensciences.fr>
2015-09-10 10:36:07 +0200

    'commit'

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = require('express')(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server)
 
+    io.configure(function () {
+      io.set("transports", ["xhr-polling"]);
+      io.set("polling duration", 10);
+    });
+
 
 // Chargement de la page index.html
 app.get('/', function (req, res) {
@@ -29,29 +34,31 @@ router.route('/robot/1')
   // gel all user
   .get(function (req, res) {
         io.sockets.emit('message',{pseudo: 'robot', message: 'A m i a Robot or a human ?'});
+        res.send(200, 'Robot call');
   });
 
   router.route('/human/1')
     // gel all user
     .get(function (req, res) {
         io.sockets.emit('message',{pseudo: 'human', message: 'human after all'});
+        res.send(200, 'Human call');
     });
 
   app.use('/api', router);
   // END ROUTING
   //////////////////////////////////
 
-io.sockets.on('connection', function (socket, pseudo) {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function(pseudo) {
-        socket.pseudo = pseudo;
-        socket.broadcast.emit('nouveau_client', pseudo);
-    });
-
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-    socket.on('message', function (message) {
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-    });
-});
+// io.sockets.on('connection', function (socket, pseudo) {
+//     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+//     socket.on('nouveau_client', function(pseudo) {
+//         socket.pseudo = pseudo;
+//         socket.broadcast.emit('nouveau_client', pseudo);
+//     });
+//
+//     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+//     socket.on('message', function (message) {
+//         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+//     });
+// });
 
 server.listen(5000);
