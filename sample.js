e72546506699722adc7e7ff75a7f0c8370e8e093
unknown <pbborel@CZS004.cityzensciences.fr>
2015-09-10 11:10:51 +0200

    'commit'

--- sample.js
+++ sample.js
@@ -53,17 +53,17 @@ router.route('/robot/1')
   // END ROUTING
   //////////////////////////////////
 
-// io.sockets.on('connection', function (socket, pseudo) {
-//     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-//     socket.on('nouveau_client', function(pseudo) {
-//         socket.pseudo = pseudo;
-//         socket.broadcast.emit('nouveau_client', pseudo);
-//     });
-//
-//     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-//     socket.on('message', function (message) {
-//         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-//     });
-// });
+io.sockets.on('connection', function (socket, pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('nouveau_client', function(pseudo) {
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    });
+});
 
 server.listen(app.get('port'));
