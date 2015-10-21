61db0b9acf21a78cc2b723d72c134d89074ba0fa
pbborel <pbborel@cityzensciences.fr>
2015-10-21 17:29:32 +0200

    log and round

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ router.route('/test/start')
 
 
   sendDataKm = setInterval(function() {
-    distance = distance + 1001;
+    distance = distance + 2001;
     sendDataTestKm(distance);
   }, 20000);
 
@@ -221,6 +221,7 @@ function addDataTable(user,distance,tklk,speedlk,hrlk){
         // we take the first time for each km
         time =  datasML[nbk][0];
       }
+      var tableKm = [time,tklk,speedlk,hrlk];
       datasML[nbk] = tableKm;
     }
 }
@@ -265,12 +266,21 @@ function secondsToTime(secs)
 {
     secs = Math.round(secs);
     var hours = Math.floor(secs / (60 * 60));
+    if(hours < 10){
+      hours = "0"+hours;
+    }
 
     var divisor_for_minutes = secs % (60 * 60);
     var minutes = Math.floor(divisor_for_minutes / 60);
+    if(minutes < 10){
+      minutes = "0"+minutes;
+    }
 
     var divisor_for_seconds = divisor_for_minutes % 60;
     var seconds = Math.ceil(divisor_for_seconds);
+    if(seconds < 10){
+      seconds = "0"+seconds;
+    }
 
     var obj = {
         "h": hours,
