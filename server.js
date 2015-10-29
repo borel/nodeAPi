94437fe74fedcfcac7c5781ea24fdbd3ab811a3a
pbborel <pbborel@cityzensciences.fr>
2015-10-29 17:32:28 +0100

    multiple fix

--- server.js
+++ server.js
@@ -130,22 +130,20 @@ router.route('/data/marker/lj/add')
 .post(function (req, res) {
   console.log("marker/lj/add=>Value");
   console.log(req.body);
-  console.log("timer");
+  console.log("timerLJ");
   console.log(timestampLJ);
 
   //store the distance , hr , speed
   if(req.body.values.distance != null){
     distanceLJ  = req.body.values.distance;
-  }else{
-    distanceLJ = 0;
   }
 
-  hrLJ  = round(req.body.values.hr,0);
+  if(req.body.values.hr != null){
+    hrLJ  = round(req.body.values.hr,0);
+  }
 
   if(req.body.values.speed != null){
     speedLJ = filterValue(speedLJ, round(req.body.values.speed * 3.6,1))
-  }else{
-    speedLJ = 0;
   }
 
   // first load
